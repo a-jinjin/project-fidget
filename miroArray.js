a0a08b3438c57219cb99c8972896c8b9bbc20b3b
a-jin <a.jinlee92@gmail.com>
2022-07-06 17:47:14 +0900

    미로 테스트 완료. 프로젝트에 적용하기

---
+++ miroArray.js
@@ -0,0 +1,29 @@
+const miroImage = [
+  [ `url('')`,
+    `url('')`,
+    `url('./miro/2-0.png')`,
+    `url('')`,
+    `url('')` ],
+  [ `url('')`,
+    `url('./miro/1-1.png')`,
+    `url('./miro/2-1.png')`,
+    `url('')`,
+    `url('./miro/4-1.png')` ],
+  [ `url('')`,
+    `url('')`,
+    `url('./miro/2-2.png')`,
+    `url('./miro/3-2.png')`,
+    `url('./miro/4-2.png')` ],
+  [ `url('')`,
+    `url('./miro/1-3.png')`,
+    `url('./miro/2-3.png')`,
+    `url('')`,
+    `url('./miro/4-3.png')`],
+  [ `url('./miro/0-4.png')`,
+    `url('./miro/1-4.png')`,
+    `url('')`,
+    `url('')`,
+    `url('')`]
+];
+
+export default miroImage;
